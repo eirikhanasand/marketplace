950dcae58d98d166522bb72e59cf2d3396190bc8
sindrhst <sindrhst@stud.ntnu.no>
2023-04-03 02:43:27 +0200

    Replaced switch cases with casting for the Kvalitet enum

--- src/bruktTing.cpp
+++ src/bruktTing.cpp
@@ -66,28 +66,7 @@ void BruktTing::settData() {
     int kategoriKvalitet = lesInt("Hvor sliten er tingen? 1 = SomNy, 2 = "
     "PentBrukt, 3 = Brukt, 4 = GodtBrukt, 5 = Sliten", 1, 5);
 
-    switch (kategoriKvalitet) {
-        case 1: {
-            kvalitet = SomNy;       
-            break;
-        }
-        case 2: {
-            kvalitet = PentBrukt;   
-            break;
-        }
-        case 3: {
-            kvalitet = Brukt;       
-            break;
-        }
-        case 4: {
-            kvalitet = GodtBrukt;   
-            break;
-        }
-        case 5: {
-            kvalitet = Sliten;      
-            break;
-        }
-    }
+    kvalitet = static_cast<Kvalitet>(kategoriKvalitet-1);
 }
 
 /**
@@ -121,32 +100,7 @@ void BruktTing::skrivMindreData() const {
 */
 void BruktTing::skrivTilFil(std::ofstream &tingFil) {
     NyTing::skrivTilFil(tingFil);
-    int kvalitetInt = 0;
-
-    switch (kvalitet) {
-        case SomNy: {
-            kvalitetInt = 1; 
-            break; 
-        }
-        case PentBrukt: {
-            kvalitetInt = 2; 
-            break;
-        } 
-        case Brukt: {
-            kvalitetInt = 3; 
-            break; 
-        }
-        case GodtBrukt: {
-            kvalitetInt = 4; 
-            break; 
-        }
-        case Sliten: {
-            kvalitetInt = 5; 
-            break;
-        }
-    }
-
-    tingFil << ' ' << aar << ' ' << kvalitetInt;
+    tingFil << ' ' << aar << ' ' << static_cast<int>(kvalitet);
 }
 
 /**
@@ -167,28 +121,7 @@ void BruktTing::settData(std::ifstream &tingFil) {
     tingFil >> kvalitetInt;
     tingFil.ignore();
 
-    switch (kvalitetInt) {
-        case 1: {
-            kvalitet = SomNy;       
-            break;
-        }
-        case 2: {
-            kvalitet = PentBrukt;   
-            break;
-        }
-        case 3: {
-            kvalitet = Brukt;       
-            break;
-        }
-        case 4: {
-            kvalitet = GodtBrukt;   
-            break;
-        }
-        case 5: {
-            kvalitet = Sliten;      
-            break;
-        }
-    }
+    kvalitet = static_cast<Kvalitet>(kvalitetInt-1);
 
     NyTing::settRestData(tingFil);
 }
