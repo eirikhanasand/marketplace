82f01a3ea0b2af68fb2510190bca9bf2b0aa5ad3
steffen <steffels@stud.no>
2023-04-08 03:33:06 +0200

    Added error message for skrivAlle

--- src/kategori.hpp
+++ src/kategori.hpp
@@ -23,6 +23,7 @@ class Kategori {
     NyTing* hentTingMedIndeks(int indeks);
     std::string hentNavn();
     int sisteTing();
+    int Kategori::antallKategorier();
     void settData(std::string navn);
     void skrivData() const;
     void skrivTingMindre() const;
--- src/kategorier.hpp
+++ src/kategorier.hpp
@@ -22,6 +22,7 @@ class Kategorier {
     Kategori *hentKategoriEntydig(std::string kategoriNavn);
     bool kategoriFinnes(std::string kategoriNavn);
     int hentAntallTing();
+    int antallKategorier();
     void kategoriHandling(char valg);
     void tingHandling(char valg);
     void lesFraFil();
